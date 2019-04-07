33f2d19641c5a1490b5d8442f7935925720ed9ed
Sophia Brandt <tech@s-br.net>
2019-04-07 11:22:11 +0200

    📦 NEW: add hound for acceptance tests, add first test

--- config/test.exs
+++ config/test.exs
@@ -3,8 +3,11 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :mango, MangoWeb.Endpoint,
-  http: [port: 4002],
-  server: false
+  http: [port: 4001],
+  server: true
+
+# Hound config
+config :hound, driver: "phantomjs"
 
 # Print only warnings and errors during test
 config :logger, level: :warn
---
+++ test/mango_web/acceptance/homepage_test.exs
@@ -0,0 +1,11 @@
+defmodule MoangoWeb.HomepageTest do
+  use ExUnit.Case
+  use Hound.Helpers
+
+  hound_session()
+
+  test "presence of featured products" do
+    navigate_to("/")
+    assert page_source() =~ "Seasonal products"
+  end
+end
