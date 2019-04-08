26f69facb33b448eee1041490b3bd407d383fcee
Sophia Brandt <tech@s-br.net>
2019-04-08 10:15:09 +0200

    📦 NEW: add first user story and implementation

--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -5,7 +5,29 @@ defmodule MoangoWeb.HomepageTest do
   hound_session()
 
   test "presence of featured products" do
+    ## GIVEN ##
+    # There are two products (Apple, Tomato) priced at 100 and 50 respectively
+    # Apple is the only seasonal product
+    #
+    ## WHEN ##
+    # I navigate to homepage
     navigate_to("/")
-    assert page_source() =~ "Seasonal products"
+
+    ## THEN ##
+    # I expect the page title to be "Seasonal Products"
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Seasonal Products"
+
+    # And I expect Apple to be in the product displayed
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Apple"
+    # And I expect its price to be displayed on the screen
+    assert product_price == "100"
+
+    # And I expect that Tomato is not present on screen
+    refute page_source() =~ "Tomato"
   end
 end
