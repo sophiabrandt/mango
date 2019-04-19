245bfefbd399bbf391b9aba781eb3a085271a533
Sophia Brandt <tech@s-br.net>
2019-04-19 09:41:28 +0200

    📦 NEW: add seed data from csv

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Mango.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:hound, "~> 1.1"}
+      {:hound, "~> 1.1"},
+      {:nimble_csv, "~> 0.6.0"}
     ]
   end
 
